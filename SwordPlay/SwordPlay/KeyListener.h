2f2acc8d68b4800554dcfed3574ad79811f7a295
sam <am.sutcliffe2@gmail.com>
2014-10-19 22:59:31 +0100

    Update of res

--- SwordPlay/SwordPlay/KeyListener.h
+++ SwordPlay/SwordPlay/KeyListener.h
@@ -6,13 +6,44 @@ using namespace irr;
 class KeyListener : public IEventReceiver
 {
 public:
+	struct SMouseState
+	{
+		core::position2di Position;
+		core::position2di PrevPosition;
+		bool PosChanged;
+		bool LeftButtonDown;
+		SMouseState() : LeftButtonDown(false) { PosChanged = false; }
+	} MouseState;
 	// This is the one method that we have to implement
 	virtual bool OnEvent(const SEvent& event)
 	{
 		// Remember whether each key is down or up
-		if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+		if (event.EventType == irr::EET_KEY_INPUT_EVENT){
 			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+		}
+		if (event.EventType == irr::EET_MOUSE_INPUT_EVENT)
+		{
+			switch (event.MouseInput.Event)
+			{
+			case EMIE_LMOUSE_PRESSED_DOWN:
+				MouseState.LeftButtonDown = true;
+				break;
 
+			case EMIE_LMOUSE_LEFT_UP:
+				MouseState.LeftButtonDown = false;
+				break;
+
+			case EMIE_MOUSE_MOVED:
+				MouseState.PosChanged = true;
+				MouseState.Position.X = event.MouseInput.X;
+				MouseState.Position.Y = event.MouseInput.Y;
+				break;
+
+			default:
+				// We won't use the wheel
+				break;
+			}
+		}
 		return false;
 	}
 
