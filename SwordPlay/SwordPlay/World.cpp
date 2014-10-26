df5f8fe5e31a708681eb718de8fdf3af82600fea
Sam <notmye@gmail.com>
2014-10-26 18:05:35 +0000

    Update

--- SwordPlay/SwordPlay/World.cpp
+++ SwordPlay/SwordPlay/World.cpp
@@ -13,15 +13,15 @@ World::World(GameManager * gm)
 	{
 		ObjectArray[i] = NULL;
 	}
-	MeshCount = 2;
+	MeshCount = 6;
 	Meshes = new IAnimatedMesh*[MeshCount];
 	//Lel
-	Meshes[0] = gm->smgr->getMesh("./Res/HeadNull.obj"); Meshes[0]->setMaterialFlag(EMF_LIGHTING, false);
+	Meshes[0] = gm->smgr->getMesh("./Res/HeadNull.x"); Meshes[0]->setMaterialFlag(EMF_LIGHTING, false);
 	Meshes[1] = gm->smgr->getMesh("./Res/Head.obj"); Meshes[1]->setMaterialFlag(EMF_LIGHTING, false);
 	Meshes[2] = gm->smgr->getMesh("./Res/Arm.obj"); Meshes[2]->setMaterialFlag(EMF_LIGHTING, false);
-	Meshes[3] = gm->smgr->getMesh("./Res/Leg.obj"); Meshes[2]->setMaterialFlag(EMF_LIGHTING, false);
-	Meshes[4] = gm->smgr->getMesh("./Res/Body.obj"); Meshes[2]->setMaterialFlag(EMF_LIGHTING, false);
-	Meshes[5] = gm->smgr->getMesh("./Res/Sword.obj"); Meshes[2]->setMaterialFlag(EMF_LIGHTING, false);
+	Meshes[3] = gm->smgr->getMesh("./Res/Leg.obj"); Meshes[3]->setMaterialFlag(EMF_LIGHTING, false);
+	Meshes[4] = gm->smgr->getMesh("./Res/Body.obj"); Meshes[4]->setMaterialFlag(EMF_LIGHTING, false);
+	Meshes[5] = gm->smgr->getMesh("./Res/Sword.obj"); Meshes[5]->setMaterialFlag(EMF_LIGHTING, false);
 
 	Camera = gm->smgr->addCameraSceneNode(0, vector3df(20, 20, 20),vector3df(0,0,0));
 	Camera->bindTargetAndRotation(true);
